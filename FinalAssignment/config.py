b3ef7124ada773c98575912e9ee56394fbb4fd10
Alvaro Alonso <112358fn@users.noreply.github.com>
2020-06-15 17:39:09 +0200

    Delete config.py

--- FinalAssignment/config.py
+++
@@ -1,4 +0,0 @@
-google_key = 'AIzaSyBUeSQ7Ts9T18uzKx6lDvAdPAWr3nP2vtk'
-foursquare_clientid = 'YVBQLDH3JY12YVERJWMJE4LDFWXNVI0P2IC1CKIYVXSXKMPZ' # your Foursquare ID
-foursquare_clientsecret = 'DM313RZNG1Y250ZM0FXQUJCYFXLBBJFXWSWIZYQQHTEU2AIW' # your Foursquare Secret
-foursquare_version = '20180605' # Foursquare API version
